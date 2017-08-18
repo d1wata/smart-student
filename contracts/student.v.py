683efb8fe3aab59ee0ca7c1234792e361a49a366
Jm Erestain <jenerestain@gmail.com>
2017-08-18 12:27:05 +0800

    Update student.v.py
    Research centric

--- contracts/student.v.py
+++ contracts/student.v.py
@@ -1,25 +1,32 @@
 student: public({
-  balanceOf: currency_value, # cashier issued Philippine Peso-pegged token for e-transaction in PSHS
-  id_num: bytes32, # Registrar specified Id number (e.g. "13-63623")
+	# Student Meta Data
+	
+  	id_num: bytes32, # Registrar specified Id number (e.g. "13-63623")
 	enrolled: bool,
-	first_name: bytes32, # Legal first name
-	last_name: bytes32, # Legal last name
-	grade: decimal[6][8] # [year][subject number code]
+	
+	# Student Profile Data
+	
+	first_name: bytes32,
+	last_name: bytes32,
+	
+	# Student Dynamic Data
+	
+	time_in: timestamp,
+	time_out: timestamp
+	
          }[address])
 
-cashier: public(address)
-reserve: num
-
-def __init__(_cashier: address):
-	self.cashier = _cashier # Central token authority
-	self.reserve = 0 	# 100,000 Pesos Starting Cash Reserve
 	
-def setFinalGrade(_grade: decimal, _subject: num, _year: num):
-	assert self.student[msg.sender].enrolled 	# Throws if student is not yet enrolled
-	self.student[msg.sender].grade[_year][_subject] = _grade
+@payable
+def checkIn(_time_in: timestamp):
+	self.student[msg.sender].time_in: _time_in
 
 @payable
-def register(_id_num: bytes32, _first_name: bytes32, _last_name: bytes32):	
+def checkOut(_time_out: timestamp):
+	self.student[msg.sender].time_out: _time_out
+
+@payable
+def enroll(_id_num: bytes32, _first_name: bytes32, _last_name: bytes32):	
 	assert not self.student[msg.sender].enrolled    # Throws if user is already enrolled
 	
 	# Adding identity data
@@ -27,14 +34,4 @@ def register(_id_num: bytes32, _first_name: bytes32, _last_name: bytes32):
 	self.student[msg.sender].first_name = _first_name
 	self.student[msg.sender].last_name = _last_name
 	self.student[msg.sender].enrolled = True
-	
-def issuePeso(_amount: num, _to: address):
-	assert msg.sender == self.cashier # Throws if user is not the cashier
-	self.reserve += _amount 	  # Add _amount to current reserve
-	
-def transfer(_to: address, _amount: currency_value):
-	assert self.student[msg.sender].balanceOf >= _amount 	# Throw if send amount greater than balance
-		  
-	self.student[msg.sender].balanceOf -= _amount
-	self.student[_to].balanceOf += _amount
 	
