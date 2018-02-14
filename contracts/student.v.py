8f90c1c0395a2277b83b1c47f44c2b0f093c7760
Jm Erestain <jenerestain@gmail.com>
2018-02-14 21:47:05 +0800

    Logging on Blockchain

--- contracts/student.v.py
+++ contracts/student.v.py
@@ -1,31 +1,32 @@
-student: public({
+user: public({
 	enrolled: bool,
 	first_name: bytes32, # Legal first name
 	last_name: bytes32, # Legal last name
          }[address])
 
-report: public({
-	longitude: num8,
-	latitude: num8,
-}[address][timestamp])
-
-cashier: public(address)
-reserve: num
-
-def __init__(_cashier: address):
-	self.owner = _cashier # Central authority
 	
-def inputReport(_grade: decimal, _subject: num, _year: num):
-	assert self.student[msg.sender].enrolled 	# Throws if student is not yet enrolled
-	self.student[msg.sender].grade[_year][_subject] = _grade
+Report: __log__({_from: indexed(address), _latitude: num256, _longitude: num256})
+
+
+def __init__(_administrator: address):
+	self.owner = _administrator # Central authority
+	
+def inputReport(_latitude, _longitude):
+	assert self.user[msg.sender].enrolled 	# Throws if student is not yet enrolled
+	self.user[msg.sender]
+	log.Report(msg.sender, convert(_latitude, 'num256'), convert(_longitude, 'num256'))  # log reporting event.
+	
+def upgradeContract(_contract):
+	# work in progress code to upgrade contract after final implementation
+
 
 @payable
 def register(_first_name: bytes32, _last_name: bytes32):	
-	assert not self.student[msg.sender].enrolled    # Throws if user is already enrolled
+	assert not self.user[msg.sender].enrolled    # Throws if user is already enrolled
 	
 	# Adding identity data
-	self.student[msg.sender].first_name = _first_name
-	self.student[msg.sender].last_name = _last_name
-	self.student[msg.sender].enrolled = True
+	self.user[msg.sender].first_name = _first_name
+	self.user[msg.sender].last_name = _last_name
+	self.user[msg.sender].enrolled = True
 	
 	
