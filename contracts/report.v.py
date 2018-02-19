c5659e5e398fb2f3d7dc1a7b536e875346644dde
Jm Erestain <jenerestain@gmail.com>
2018-02-19 11:52:31 +0800

    Code fixes

--- contracts/report.v.py
+++ contracts/report.v.py
@@ -1,29 +1,21 @@
+Report: __log__({_from: indexed(address), _latitude: indexed(num256), _longitude: indexed(num256)})
+
 user: public({
 	enrolled: bool,
 	first_name: bytes32, # Legal first name
 	last_name: bytes32, # Legal last name
-	bri: decimal, # Blockchain Report Index
          }[address])
-	
-report: public({
-	longitude: num16,
-	latitude: num16,
-}[num][address])
 
-cashier: public(address)
-Report: __log__({latitude: num16, longitude: num16, arg3: indexed(address)})
-
-
-def __init__(_cashier: address):
-	self.owner = _cashier # Central authority
+owner: address
 
 @public
-def inputReport(_longitude, _latitude):
+def __init__(_administrator: address):
+	self.owner = _administrator # Central authority
+
+@public
+def inputReport(_latitude: num256, _longitude: num256):
 	assert self.user[msg.sender].enrolled 	# Throws if student is not yet enrolled
-	self.report.longitude[bri][msg.sender] = _longitude
-	self.report.latitude[bri][msg.sender] = _latitude
-	self.user[msg.sender].bri += 1
-	log.Report(_longitude, _latitude, msg.sender)
+	log.Report(msg.sender, _latitude, _longitude)  # log reporting event.
 
 @public
 @payable
@@ -34,6 +26,3 @@ def register(_first_name: bytes32, _last_name: bytes32):
 	self.user[msg.sender].first_name = _first_name
 	self.user[msg.sender].last_name = _last_name
 	self.user[msg.sender].enrolled = True
-	self.user[msg.sender].bri = 0
-	
-	
