387d8aba7206dd61d8b776a8c971e7149f759119
Jm Erestain <jenerestain@gmail.com>
2019-03-04 12:33:42 +0800

    Update abi.js

--- js/abi.js
+++ js/abi.js
@@ -1,1 +1,136 @@
-abiArray = [{"name": "Report", "inputs": [{"type": "address", "name": "_from", "indexed": true}, {"type": "uint256", "name": "_latitude", "indexed": false}, {"type": "uint256", "name": "_longitude", "indexed": false}], "anonymous": false, "type": "event"}, {"name": "isRegistered", "outputs": [{"type": "bool", "name": "out"}], "inputs": [{"type": "address", "name": "addr"}], "constant": true, "payable": false, "type": "function", "gas": 693}, {"name": "inputReport", "outputs": [], "inputs": [{"type": "uint256", "name": "_latitude"}, {"type": "uint256", "name": "_longitude"}], "constant": false, "payable": false, "type": "function", "gas": 2607}, {"name": "register", "outputs": [], "inputs": [{"type": "bytes32", "name": "_first_name"}, {"type": "bytes32", "name": "_last_name"}], "constant": false, "payable": false, "type": "function", "gas": 61275}, {"name": "user__enrolled", "outputs": [{"type": "bool", "name": "out"}], "inputs": [{"type": "address", "name": "arg0"}], "constant": true, "payable": false, "type": "function", "gas": 783}, {"name": "user__first_name", "outputs": [{"type": "bytes32", "name": "out"}], "inputs": [{"type": "address", "name": "arg0"}], "constant": true, "payable": false, "type": "function", "gas": 819}, {"name": "user__last_name", "outputs": [{"type": "bytes32", "name": "out"}], "inputs": [{"type": "address", "name": "arg0"}], "constant": true, "payable": false, "type": "function", "gas": 849}]
+abiArray = [
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "_latitude",
+                "type": "fixed168x10"
+            },
+            {
+                "indexed": false,
+                "name": "_longitude",
+                "type": "fixed168x10"
+            }
+        ],
+        "name": "Report",
+        "type": "event"
+    },
+    {
+        "constant": false,
+        "gas": 727,
+        "inputs": [
+            {
+                "name": "addr",
+                "type": "address"
+            }
+        ],
+        "name": "isRegistered",
+        "outputs": [
+            {
+                "name": "out",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "gas": 3111,
+        "inputs": [
+            {
+                "name": "_latitude",
+                "type": "fixed168x10"
+            },
+            {
+                "name": "_longitude",
+                "type": "fixed168x10"
+            }
+        ],
+        "name": "inputReport",
+        "outputs": [],
+        "payable": false,
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "gas": 106411,
+        "inputs": [
+            {
+                "name": "_first_name",
+                "type": "bytes32"
+            },
+            {
+                "name": "_last_name",
+                "type": "bytes32"
+            }
+        ],
+        "name": "register",
+        "outputs": [],
+        "payable": false,
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "gas": 817,
+        "inputs": [
+            {
+                "name": "arg0",
+                "type": "address"
+            }
+        ],
+        "name": "user__enrolled",
+        "outputs": [
+            {
+                "name": "out",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "gas": 853,
+        "inputs": [
+            {
+                "name": "arg0",
+                "type": "address"
+            }
+        ],
+        "name": "user__first_name",
+        "outputs": [
+            {
+                "name": "out",
+                "type": "bytes32"
+            }
+        ],
+        "payable": false,
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "gas": 883,
+        "inputs": [
+            {
+                "name": "arg0",
+                "type": "address"
+            }
+        ],
+        "name": "user__last_name",
+        "outputs": [
+            {
+                "name": "out",
+                "type": "bytes32"
+            }
+        ],
+        "payable": false,
+        "type": "function"
+    }
+]
